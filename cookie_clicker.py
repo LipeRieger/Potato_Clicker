0dd550a8f87a58a548a4f47ba8b15ae282d30351
Eduardo-Tramontina <134700477+Eduardo-Tramontina@users.noreply.github.com>
2024-08-22 09:29:41 -0300

    Descrição
    colocamos comentários nos códigos, explicando como funciona e onde utilizar cada um

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -1,20 +1,28 @@
+#IMPORTANDO BIBLIOTECA URSINA
 from ursina import *
 
+#INICIALIZA O AMBIENTE GRAFICO
 app = Ursina()
 
+#O NUMERO DE BATATAS COMEÇA EM 0
 potatos = 0
+#STATUS DE POSSE DAS FABRICAS
 industry1_buyed = False
 industry2_buyed = False
+#DEFINE/COMFIGURA O FUNDO DO APLICATIVO
 background = Entity(model='quad', texture='Background.png', scale=(20,10))
+#DEFINE O CONTADOR DE BATATAS
 counter = Text(text='0', y=0.25, scale=2, origin=(0, -1), background=True)
+#CONFIGURA A BATATA (IMG) COMO UM BOTÃO
 button = Button(texture='Potato.png', scale=0.3, color=color.white)
+#CONFIGURA AS FABRICAS (IMG) COMO UM BOTÃO
 industry1 = Button(cost=10, x=0.7, y=-0.06, scale=0.2, texture='Indústria1.png', disabled=True)
 industry2 = Button(cost=100, x=-0.65,scale=0.3, texture='Indústria2.png', disabled=True)
+#CONFIGURA A DESCRIÇÃO DAS FABRICAS
 industry1.tooltip = Tooltip(f'<gold>Potato Factory\n<default>Earn 1 potato every second.\nCosts {industry1.cost} potatos.')
 industry2.tooltip = Tooltip(f'<gold>Potato Factory\n<default>Earn 10 potato every second.\nCosts {industry2.cost} potatos.')
 
-#GERADOR MANUAL
-
+#FUNÇÃO GERADOR MANUAL DE BATATAS
 def button_click():
     global potatos
     potatos += 1
@@ -23,10 +31,10 @@ def button_click():
     counter.text= str(potatos)
     counter.background= True
 
+#EXECUTA A FUNÇÃO AO CLICAR COM O MOUSE NA BATATA
 button.on_click = button_click
 
-#INDÚSTRIA 1
-
+#FUNÇÃO PARA COMPRAR A INDÚSTRIA 1
 def buy_industry1():
     global potatos
     global industry1_buyed
@@ -36,8 +44,10 @@ def buy_industry1():
         counter.text = str(potatos)
         invoke(generate_gold_industry1, 1, 1)
 
+#EXECUTA A FUNÇÃO AO CLICAR COM O MOUSE NA INDUSTRIA 1 
 industry1.on_click = buy_industry1
 
+#FUNÇÃO GERADORA DE INDUSTRIA 1
 def generate_gold_industry1(value=1, interval=1):
     global potatos
     potatos += 1
@@ -47,8 +57,7 @@ def generate_gold_industry1(value=1, interval=1):
     counter.background= True
     invoke(generate_gold_industry1, value, delay=interval)
 
-#INDÚSTRIA 2
-
+#FUNÇÃO DE COMPRA DA INDÚSTRIA 2
 def buy_industry2():
     global potatos
     global industry2_buyed
@@ -58,8 +67,10 @@ def buy_industry2():
         counter.text = str(potatos)
         invoke(generate_gold_industry2, 1, 1)
 
+#EXECUTA A FUNÇÃO AO CLICAR COM O MOUSE NA INDUSTRIA 2
 industry2.on_click = buy_industry2
 
+#FUNÇÃO GERADORA DA INDUSTRIA 2
 def generate_gold_industry2(value=1, interval=1):
     global potatos
     potatos += 10
@@ -69,8 +80,7 @@ def generate_gold_industry2(value=1, interval=1):
     counter.background= True
     invoke(generate_gold_industry2, value, delay=interval)
 
-#ATUALIZAÇÃO
-
+#FUNÇÃO DE ATUALIZAÇÃO DE DADOS
 def update():
     global potatos
     global industry1
@@ -82,5 +92,5 @@ def update():
         if potatos >= b.cost:
             b.color = color.white
     
-
+#EXECUTAR O CODIGO
 app.run()
